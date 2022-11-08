47c56f26efe791a09d9808c5122c9e781eda5cc0
ChrisKnowsDev <chrisharley81@gmail.com>
2022-11-08 14:33:44 -0500

    server setup and running

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello from server')
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Server')
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Serve listening on port: ${port}`))
