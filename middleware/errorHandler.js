fa7d237a044e50388b7092d0fb3acfbc42369c01
ChrisKnowsDev <chrisharley81@gmail.com>
2022-11-09 10:39:52 -0500

    error handler func working

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+// https://expressjs.com/en/guide/error-handling.html
+// rules of express error handler
+// accept 4 parameters, first one error
+// writing these four parameters, express knows it's going to be an error handler
+export default function errorHandlerMiddleware(err, req, res, next) {
+  console.log(err)
+  res.status(500).json({ msg: 'There was an error' })
+}
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from 'express'
 const app = express()
-
-// middleware
 import notFoundMiddleware from './middleware/notFound.js'
+import errorHandlerMiddleware from './middleware/errorHandler.js'
 
 app.get('/', (req, res) => {
+  throw new Error('error')
   res.send('Server')
 })
 
+// Middleware
+// looks for request that do not match any of your routes
 app.use(notFoundMiddleware)
+// looks for errors happening in our existing route
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 8000
 
