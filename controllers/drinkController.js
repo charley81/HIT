e166060f8771bf97934f38e4663617bbe45b3f6b
ChrisKnowsDev <chrisharley81@gmail.com>
2022-11-15 08:47:47 -0500

    create drink controller complete and saving new drink to DB

--- controllers/drinkController.js
+++ controllers/drinkController.js
@@ -1,34 +1,55 @@
+import Drink from '../models/Drinks.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, NotFoundError } from '../errors/index.js'
+
 // @desc create a drink
 // @route POST /api/v1/drinks
 // @access private
-export function createDrink(req, res) {
-  res.send('create drink')
+export async function createDrink(req, res) {
+  const { name, type, breweryLocation, breweryName, thoughts, rating } =
+    req.body
+
+  if (
+    !name ||
+    !type ||
+    !breweryLocation ||
+    !breweryName ||
+    !thoughts ||
+    !rating
+  ) {
+    throw new BadRequestError('please provide all values')
+  }
+
+  req.body.createdBy = req.user.userId
+
+  const drink = await Drink.create(req.body)
+  res.status(StatusCodes.CREATED).json({ drink })
 }
 
 // @desc delete a drink
 // @route DELETE /api/v1/drinks/:id
 // @access private
-export function deleteDrink(req, res) {
+export async function deleteDrink(req, res) {
   res.send('delete drink')
 }
 
 // @desc get all drinks
 // @route GET /api/v1/drinks
 // @access private
-export function getAllDrinks(req, res) {
+export async function getAllDrinks(req, res) {
   res.send('get all drinks')
 }
 
 // @desc update a drink
 // @route PATCH /api/v1/drinks/:id
 // @access private
-export function updateDrink(req, res) {
+export async function updateDrink(req, res) {
   res.send('update drink')
 }
 
 // @desc show user drink stats
 // @route GET /api/v1/drinks/stats
 // @access private
-export function showInfo(req, res) {
+export async function showInfo(req, res) {
   res.send('show info')
 }
