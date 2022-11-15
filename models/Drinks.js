504a69c2b52f9621421683afa4f3d4ffd7ecb163
ChrisKnowsDev <chrisharley81@gmail.com>
2022-11-15 08:26:31 -0500

    created drinks model

---
+++ models/Drinks.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+import { Schema } from 'mongoose'
+
+const DrinksSchema = new Schema(
+  {
+    /**
+     * name, type, location, brewery name, thoughts, rating(number), date added
+     */
+    name: {
+      type: String,
+      required: [true, 'please provide a drink name'],
+      maxLength: 15
+    },
+    type: {
+      type: String,
+      required: [true, 'please provide a drink type'],
+      maxLength: 15,
+      default: 'pilsner'
+    },
+    breweryLocation: {
+      type: String,
+      required: [true, 'please provide a location'],
+      default: 'my city'
+    },
+    breweryName: {
+      type: String,
+      required: [true, 'please provide a brewery name'],
+      default: 'brewery name'
+    },
+    thoughts: {
+      type: String,
+      required: [true, 'please provide your thoughts about this drink'],
+      default: 'I love this beer'
+    },
+    rating: {
+      type: Number,
+      required: [true, 'please provide a rating'],
+      min: 1,
+      max: 10,
+      default: 10
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'please provide a user']
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default mongoose.model('Drink', DrinksSchema)
