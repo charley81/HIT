bead0c588e37016d309451e6501d71497a927448
ChrisKnowsDev <chrisharley81@gmail.com>
2022-11-16 19:42:47 -0500

    pagination server complete

--- controllers/drinkController.js
+++ controllers/drinkController.js
@@ -88,11 +88,20 @@ export async function getAllDrinks(req, res) {
     result = result.sort('-drinkName')
   }
 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const drinks = await result
 
-  res
-    .status(StatusCodes.OK)
-    .json({ drinks, totalDrinks: drinks.length, numOfPages: 1 })
+  // get the total number of docs for a certain user
+  const totalDrinks = await Drink.countDocuments(queryObject)
+  // take the total docs and divide by limit to get num of pages and round up so we don't have a half page
+  const totalPages = Math.ceil(totalDrinks / limit)
+
+  res.status(StatusCodes.OK).json({ drinks, totalDrinks, totalPages })
 }
 
 // @desc update a drink
